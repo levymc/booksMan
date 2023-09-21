0c8405768d7d1f8008a9264046dd7d7880c2cda2
Levy <levymcruz@gmail.com>
2023-09-21 20:20:32 -0300

    finishing

--- src/protocols/book.ts
+++ src/protocols/book.ts
@@ -1,12 +1,3 @@
-export type Book = {
-  id: number;
-  title: string;
-  author: string;
-  publisher: string;
-  purchaseDate: Date;
-  read: boolean;
-  review: string;
-  grade: number;
-};
+import { Books } from "@prisma/client";
 
-export type CreateBook = Omit<Book, "id" | "read" | "review" | "grade">;
+export type CreateBook = Omit<Books, "id" | "read" | "review" | "grade">;
--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -1,44 +1,33 @@
-import { Book, CreateBook } from "../protocols/book";
+import { CreateBook } from "../protocols/book";
 import { CreateReview } from "../protocols/review";
+import prisma from "../database/index";
 
-import connection from "../database";
 
 export async function getBooks() {
-  const query = `SELECT * FROM books`;
-  const result = await connection.query<Book>(query);
-  return result.rows;
+  return prisma.books.findMany();
 }
 
 export async function getBook(id: number) {
-  const query = `SELECT * FROM books WHERE id = $1`;
-  const result = await connection.query<Book>(query, [id]);
-  return result.rows[0];
+  return prisma.books.findUnique({
+    where: {id}
+  })
 }
 
 export async function createBook(book: CreateBook) {
-  const { title, author, publisher, purchaseDate } = book;
-  const query = `
-    INSERT INTO books (title, author, publisher, "purchaseDate")
-    VALUES ($1, $2, $3, $4)`;
-
-  const result = await connection.query(query, [
-    title, author, publisher, purchaseDate
-  ]);
-
-  return result.rowCount;
+  return prisma.books.create({
+    data: book
+  })
 }
 
 export async function reviewBook(bookReview: CreateReview) {
   const { bookId, grade, review } = bookReview;
-  const query = `
-    UPDATE books 
-    SET
-      grade = $1,
-      review = $2,
-      read = true 
-    WHERE id = $3
-  `;
-
-  const result = await connection.query(query, [grade, review, bookId]);
-  return result.rowCount;
+  return prisma.books.update({
+    data: {
+      grade,
+      review
+    },
+    where: {
+      id: bookId
+    }
+  })
 }
